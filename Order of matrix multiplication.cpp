3cd17e5c6646b28e3196b9214b3cc6a0b1fcab7a
Timofey <135414755+ONion-Start@users.noreply.github.com>
2024-11-12 17:26:19 +0300

    Update Order of matrix multiplication.cpp

--- Order of matrix multiplication.cpp
+++ Order of matrix multiplication.cpp
@@ -22,3 +22,21 @@ int main() {
     out.close();
     return 0;
 }
+int matrixChainMultiplication(std::vector<int>& dimensions) {
+    int n = dimensions.size() - 1;
+    std::vector<std::vector<int>> dp(n, std::vector<int>(n, 0));
+
+    for (int length = 2; length <= n; ++length) {
+        for (int i = 0; i < n - length + 1; ++i) {
+            int j = i + length - 1;
+            dp[i][j] = std::numeric_limits<int>::max();
+            for (int k = i; k < j; ++k) {
+                int cost = dp[i][k] + dp[k+1][j] + dimensions[i] * dimensions[k+1] * dimensions[j+1];
+                if (cost < dp[i][j]) {
+                    dp[i][j] = cost;
+                }
+            }
+        }
+    }
+    return dp[0][n-1];
+}
